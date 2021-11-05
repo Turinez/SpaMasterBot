5de7a16a4df04430e13a1f323862321f9f01277a
SuperProgery <85991199+SuperProgery@users.noreply.github.com>
2021-11-05 18:45:43 +0300

    Update products_list.py

--- products_list.py
+++ products_list.py
@@ -1,14 +1,50 @@
+from collections import namedtuple
+
+
 def sort_key(e):
     return e[3]
-def to_string(product_list):
 
+
+def to_string(product_list):
     return f"Косметика {str(product_list[1])} фирмы {str(product_list[0])} стоимостью {str(product_list[3])} рублей"
 
-def search():
-    sorted_products = sorted(products, key=sort_key)
+
+product = namedtuple('Product', ('manufacturer', 'series', 'purpose', 'price'))
+
+
+def search(professional=None):
+    # selected_products = ... products
+    if professional is None:
+        selected_products = products
+    elif professional is True:
+        selected_products = []
+        for i in products:
+            if i.series == 'профессиональная':
+                selected_products.append(i)
+        # бежим по списку продуктов и если в нем есть во втором
+        # столбце записана профессиональная косметика, то мы заносим
+        # все строчку в свой новый список
+
+    sorted_products = sorted(selected_products, key=sort_key)
     to_print = sorted_products[0:10]
     return to_print
 
+
+
+def get_prof_cos():
+
+    indices = [element for element in products if element[1] == 'профессиональная']
+    print(indices)
+    return indices
+
+def home():
+    home_products = list(filter(lambda x: x[1] == 'домашняя', products))
+    print(*home_products, sep="\n")
+
+
+def proff():
+    proff_products = list(filter(lambda x: x[1] == 'профессиональная', products))
+    print(*proff_products, sep="\n")
 
 
 
